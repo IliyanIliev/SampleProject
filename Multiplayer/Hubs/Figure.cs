701352236913758bc78f7669925219945003180f
Iliyan Iliev <iliyanbiserov@gmail.com>
2013-08-12 21:56:11 +0300

    lele male

---
+++ Multiplayer/Hubs/Figure.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Multiplayer.Hubs
+{
+    public class Figure:Hub
+    {
+        public void Move(string direction)
+        {
+            switch (direction)
+            {
+                case "left": Clients.All.moveLeft(); break;
+                case "right": Clients.All.moveRight(); break;
+                case "up": Clients.All.moveUp(); break;
+                case "down": Clients.All.moveDown(); break;
+                case "jump": Clients.All.jump(); break;
+            }
+            
+        }
+    }
+}
